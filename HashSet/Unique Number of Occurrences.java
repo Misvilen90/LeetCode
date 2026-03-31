c70316e229d80e49e623fcff0dde810903a9ea1f
Priyanka T <priyankathiyagu11@gmail.com>
2026-03-31 15:25:32 +0530

    Create Unique Number of Occurrences.java

---
+++ HashSet/Unique Number of Occurrences.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean uniqueOccurrences(int[] arr) {
+        Map<Integer,Integer> map = new HashMap<>();
+        for(int num:arr){
+            map.put(num,map.getOrDefault(num,0)+1);
+        }
+        Set<Integer> set = new HashSet<>();
+        for(int i : map.values()){
+            if(set.contains(i)){
+                return false;
+            }
+            set.add(i);
+        }
+        return true;
+    }
+}
