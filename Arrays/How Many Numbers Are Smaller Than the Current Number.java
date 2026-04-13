46642e93904ad5af89f3b532e9a3f0a3f06f4083
Priyanka T <priyankathiyagu11@gmail.com>
2026-04-13 22:13:13 +0530

    Implement method to count smaller numbers

---
+++ Arrays/How Many Numbers Are Smaller Than the Current Number.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[] smallerNumbersThanCurrent(int[] nums) {
+        int[] freq = new int[101];
+
+        // Count frequency
+        for (int num : nums) {
+            freq[num]++;
+        }
+
+        // Prefix sum (how many numbers smaller)
+        for (int i = 1; i < 101; i++) {
+            freq[i] += freq[i - 1];
+        }
+
+        int[] result = new int[nums.length];
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                result[i] = 0;
+            } else {
+                result[i] = freq[nums[i] - 1];
+            }
+        }
+
+        return result;
+    }
+}
